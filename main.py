c91494c17179b7f02e9da9bdd876809580ebab05
BeataNowakowska <the.beata.nowakowska@gmail.com>
2025-04-25 14:42:45 +0000

    Logging on branch

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from log import Logger
 
 logger = Logger("main_log.txt")
 
-logger.log("Program started.")
+logger.log("Program started...")
 print("Mam na imię Mateusz")
+logger.log("Program ends...")
